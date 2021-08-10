ae9d98f4741144e4e12d5a3f1ae7a31224f81fd0
Ayo <bimboladxtreme@gmail.com>
2021-08-10 16:04:44 +0100

    App is done

--- src/App.js
+++ src/App.js
@@ -3,38 +3,23 @@ import { createStore } from "redux";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
-import { DECREASE, INCREASE } from "./actions";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
 
 const initialStore = {
-	count: 0,
-	name: "Ayo",
+	cart: cartItems,
+	amount: 5,
+	total: 105,
 };
 
-function reducer(state, action) {
-	console.log({ state, action });
-	if (action.type === DECREASE) {
-		return { ...state, count: state.count - 1 };
-	}
-	if (action.type === INCREASE) {
-		return { ...state, count: state.count + 1 };
-	}
-
-	return state;
-}
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: DECREASE });
-store.dispatch({ type: INCREASE });
-store.dispatch({ type: INCREASE });
-
-console.log(store.getState());
 
 function App() {
-	// cart setup
 	return (
-		<main>
-			<Navbar cart={store.getState()} />
-			<CartContainer cart={cartItems} />
-		</main>
+		<Provider store={store}>
+			<Navbar />
+			<CartContainer />
+		</Provider>
 	);
 }
 
