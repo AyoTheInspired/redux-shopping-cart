5ed2fba5f555ddeb563bfefb9aa236750412c1de
Ayo <bimboladxtreme@gmail.com>
2021-08-10 08:01:05 +0100

    Installed react-redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from "react";
-// components
+import { createStore } from "redux";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
 import cartItems from "./cart-items";
-// redux stuff
+
+const initialStore = {
+	count: 78,
+};
+
+function reducer(state, action) {
+	console.log({ state, action });
+	if (action.type === "DECREASE") {
+		return { count: state.count - 1 };
+		console.log("Hey, it worked");
+	}
+	return state;
+}
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "RESET" });
+console.log(store.getState());
 
 function App() {
-  // cart setup
-
-  return (
-    <main>
-      <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
-  );
+	// cart setup
+	return (
+		<main>
+			<Navbar cart={store.getState()} />
+			<CartContainer cart={cartItems} />
+		</main>
+	);
 }
 
 export default App;
