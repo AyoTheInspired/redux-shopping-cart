0474f374d39da791a32b879fe8feff7b6a22ac54
Ayo <bimboladxtreme@gmail.com>
2021-08-10 08:38:51 +0100

    Added actions file

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import { createStore } from "redux";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
+import { DECREASE, INCREASE } from "./actions";
 
 const initialStore = {
-	count: 78,
+	count: 0,
+	name: "Ayo",
 };
 
 function reducer(state, action) {
 	console.log({ state, action });
-	if (action.type === "DECREASE") {
-		return { count: state.count - 1 };
-		console.log("Hey, it worked");
+	if (action.type === DECREASE) {
+		return { ...state, count: state.count - 1 };
 	}
+	if (action.type === INCREASE) {
+		return { ...state, count: state.count + 1 };
+	}
+
 	return state;
 }
 const store = createStore(reducer, initialStore);
-store.dispatch({ type: "DECREASE" });
-store.dispatch({ type: "INCREASE" });
-store.dispatch({ type: "RESET" });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: INCREASE });
+
 console.log(store.getState());
 
 function App() {
